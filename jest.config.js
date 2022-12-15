a7436480b9c3250a057bbc66fef1fa77e41d500d
alisson <alissoneatena1@gmail.com>
2022-12-15 00:11:29 -0300

    add jest and react-testing/library

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,13 @@ const customJestConfig = {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+    // Add any custom reporters and enable coverage
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    coverageDirectory: 'coverage',
+    coveragePathIgnorePatterns: ['/node_modules/', '/.next/', '/coverage/', '/jest.setup.js'],
+
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async

44c4636bfa21a700c993e1297d5e15ee9cbc0c91
alisson <alissoneatena1@gmail.com>
2022-12-16 16:12:33 -0300

    Ending boilerplate

--- next.config.js
+++ next.config.js
@@ -4,9 +4,14 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     styledComponents: true,
-    
+
   },
 
 }
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development"
+})
+
+module.exports = withPWA(nextConfig)
